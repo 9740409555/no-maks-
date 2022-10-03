c336337d0d803e83bf81b009a2822e86f5d1a5dc
9740409555 <98459697+9740409555@users.noreply.github.com>
2022-10-03 18:51:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5xR8jDDRR/model.json',modelLoaded);
+
+function modelLoaded() 
+
+{
+  console.log ("model Loaded!");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+       console.error(error) ;
+    } else {
+console.log(results);
+document.getElementById("status").innerHTML = results[0].label;
+
+
+if (results[0].label == "proper mask") {
+    document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+
+}
+if (results[0].label == "no mask") {
+    document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+    
+}
+    }
+}
